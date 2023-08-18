a3e5246d644aef8ba086dc6cd849b770111cd59f
Fabiano Guimarães <guimaraes_fabiano11@hotmail.com>
2023-08-17 22:42:54 -0300

    feat: registration form creation

---
+++ src/components/SubscribeForm.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useState } from "react";
+
+export default function SubscribeForm() {
+  const [email, setEmail] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setEmail("")
+    alert("Email cadastrado com sucesso!")
+  }
+
+  return (
+    <form
+      className="flex justify-center gap-4 p-4"
+      onSubmit={handleSubmit}
+    >
+      <input 
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Digite seu e-mail principal"
+        className="bg-slate-800 p-3 rounded"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button className="bg-sky-700 p-3 rounded">
+        Se inscrever
+      </button>
+    </form>
+  )
+}
